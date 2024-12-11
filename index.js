31d9f3ce970244cb72b57ef5ea9eeef770bd439f
Salvador Amba Jr <46850774+Buko-pie@users.noreply.github.com>
2024-12-11 14:33:52 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.listen(4000, () => {
+    console.log("CRUD BACKEND LISTENING @ PORT 4000");
+})
